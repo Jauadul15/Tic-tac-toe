ed037a28a2b1c45d004268ab6c9daa80c0ce994b
Jauadul15 <rashik.gmh@gmail.com>
2026-03-01 16:49:57 +0600

    adding interactivity like button X function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
-function Square({value}){
+import {useState} from 'react';
+
+function Square(){
+  const [value,setValue]=useState(null)
+  function handleClick(){
+    setValue('X');
+  }
   return (
-    <button className="bg-white border border-gray-500 h-12 w-12 m-1 leading-9 text-lg">{value}</button>
+    <button className="bg-white border border-gray-500 h-12 w-12 m-1 leading-9 text-lg"
+    onClick={handleClick}
+    >
+      {value}
+      </button>
   )
 }
 
@@ -8,18 +18,18 @@ function Square({value}){
 export default function Board(){
   return(
     <>
-     <div>
+     <div className='flex'>
         <Square value="1"/>
         <Square value="2"/>
         <Square value="3"/>
       </div>
 
-      <div>
+      <div className='flex'>
         <Square value="4"/>
         <Square value="5"/>
         <Square value="6"/>
       </div>
-      <div>
+      <div className='flex'>
         <Square value="7"/>
         <Square value="8"/>
         <Square value="9"/>
